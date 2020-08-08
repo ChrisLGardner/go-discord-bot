d1383d18e0f2332cc40257ed13fc970b6544067a
Chris Gardner <chris.gardner@section8games.co.uk>
2020-08-08 16:00:52 +0000

    add example router

--- go.mod
+++ go.mod
@@ -3,12 +3,11 @@ module github.com/chrislgardner/go-discord-bot
 go 1.14
 
 require (
+	github.com/Lukaesebrot/dgc v1.0.6 // indirect
 	github.com/andersfylling/disgord v0.18.0
-	github.com/golang/protobuf v1.3.5 // indirect
+	github.com/bwmarrin/discordgo v0.22.0 // indirect
+	github.com/honeycombio/beeline-go v0.6.1 // indirect
 	github.com/kr/pretty v0.2.0 // indirect
-	github.com/stretchr/testify v1.5.1 // indirect
-	golang.org/x/crypto v0.0.0-20200302210943-78000ba7a073 // indirect
 	golang.org/x/net v0.0.0-20200324143707-d3edc9973b7e // indirect
 	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
-	gopkg.in/yaml.v2 v2.2.8 // indirect
 )
--- main.go
+++ main.go
@@ -1,29 +1,124 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"os"
+	"os/signal"
+	"strconv"
+	"syscall"
+	"time"
 
-	"github.com/andersfylling/disgord"
+	"github.com/Lukaesebrot/dgc"
+	"github.com/bwmarrin/discordgo"
+	"github.com/honeycombio/beeline-go"
 )
 
-func printMessage(session disgord.Session, evt *disgord.MessageCreate) {
-	msg := evt.Message
-	fmt.Println(msg.Author.String() + ": " + msg.Content) // Anders#7248{435358734985}: Hello there
+func main() {
+
+	beeline.Init(beeline.Config{
+		WriteKey: os.Getenv("HONEYCOMB_KEY"),
+		Dataset:  os.Getenv("HONEYCOMB_DATASET"),
+	})
+
+	defer beeline.Close()
+
+	// Open a simple Discord session
+	token := os.Getenv("DISCORD_TOKEN")
+	session, err := discordgo.New("Bot " + token)
+	if err != nil {
+		panic(err)
+	}
+	err = session.Open()
+	if err != nil {
+		panic(err)
+	}
+
+	// Wait for the user to cancel the process
+	defer func() {
+		sc := make(chan os.Signal, 1)
+		signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+		<-sc
+	}()
+
+	// Create a dgc router
+	// NOTE: The dgc.Create function makes sure all the maps get initialized
+	router := dgc.Create(&dgc.Router{
+		// We will allow '!' and 'example!' as the bot prefixes
+		Prefixes: []string{
+			"!",
+		},
+
+		// We will ignore the prefix case, so 'eXaMpLe!' is also a valid prefix
+		IgnorePrefixCase: true,
+
+		// We don't want bots to be able to execute our commands
+		BotsAllowed: false,
+
+		// We may initialize our commands in here, but we will use the corresponding method later on
+		Commands: []*dgc.Command{},
+
+		// We may inject our middlewares in here, but we will also use the corresponding method later on
+		Middlewares: []dgc.Middleware{},
+
+		// This handler gets called if the bot just got pinged (no argument provided)
+		PingHandler: func(ctx *dgc.Ctx) {
+			ctx.RespondText("Pong!")
+		},
+	})
+
+	// Register the default help command
+	router.RegisterDefaultHelpCommand(session, nil)
+
+	// Register a simple middleware that injects a custom object
+	router.RegisterMiddleware(func(next dgc.ExecutionHandler) dgc.ExecutionHandler {
+		return func(ctx *dgc.Ctx) {
+			// Inject a custom object into the context
+			ctx.CustomObjects.Set("myObject", 69)
+
+			// You can retrieve the object like this
+			obj := ctx.CustomObjects.MustGet("myObject").(int)
+			fmt.Println(obj)
+
+			// Call the next execution handler
+			next(ctx)
+		}
+	})
+
+	// Register a simple command that responds with our custom object
+	router.RegisterCmd(&dgc.Command{
+		// We want to use 'obj' as the primary name of the command
+		Name: "obj",
+
+		// We also want the command to get triggered with the 'object' alias
+		Aliases: []string{
+			"object",
+		},
+
+		// These fields get displayed in the default help messages
+		Description: "Responds with the injected custom object",
+		Usage:       "obj",
+		Example:     "obj",
+
+		// You can assign custom flags to a command to use them in middlewares
+		Flags: []string{},
+
+		// We want to ignore the command case
+		IgnoreCase: true,
+
+		// You may define sub commands in here
+		SubCommands: []*dgc.Command{},
+
+		// We want the user to be able to execute this command once in five seconds and the cleanup interval shpuld be one second
+		RateLimiter: dgc.NewRateLimiter(5*time.Second, 1*time.Second, func(ctx *dgc.Ctx) {
+			ctx.RespondText("You are being rate limited!")
+		}),
+
+		// Now we want to define the command handler
+		Handler: objCommand,
+	})
 }
 
-func main() {
-	// see docs/examples/* for more information about configuration and use cases
-	client := disgord.New(disgord.Config{
-		BotToken: os.Getenv("DISGORD_TOKEN"),
-	})
-	// connect, and stay connected until a system interrupt takes place
-	defer client.StayConnectedUntilInterrupted(context.Background())
-
-	// create a handler and bind it to new message events
-	// handlers/listener are run in sequence if you register more than one
-	// so you should not need to worry about locking your objects unless you do any
-	// parallel computing with said objects
-	client.On(disgord.EvtMessageCreate, printMessage)
+func objCommand(ctx *dgc.Ctx) {
+	// Respond with the just set custom object
+	ctx.RespondText(strconv.Itoa(ctx.CustomObjects.MustGet("myObject").(int)))
 }
