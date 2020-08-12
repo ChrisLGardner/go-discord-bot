fad063c03b1859ed782b65bacdb18e969d8fee41
Chris Gardner <chris.gardner@section8games.co.uk>
2020-08-12 20:24:23 +0000

    Add initial honeycomb integration

--- main.go
+++ main.go
@@ -1,17 +1,21 @@
 package main
 
 import (
-	"fmt"
+	"context"
 	"os"
 	"os/signal"
-	"strconv"
 	"syscall"
-	"time"
 
-	"github.com/Lukaesebrot/dgc"
 	"github.com/bwmarrin/discordgo"
 	"github.com/honeycombio/beeline-go"
+	"github.com/honeycombio/beeline-go/trace"
 )
+
+//MessageEvent Wrapper type to track context with message body
+type MessageEvent struct {
+	Message *discordgo.Message
+	Context context.Context
+}
 
 func main() {
 
@@ -40,85 +44,90 @@ func main() {
 		<-sc
 	}()
 
-	// Create a dgc router
-	// NOTE: The dgc.Create function makes sure all the maps get initialized
-	router := dgc.Create(&dgc.Router{
-		// We will allow '!' and 'example!' as the bot prefixes
-		Prefixes: []string{
-			"!",
-		},
+	session.Identify.Intents = discordgo.MakeIntent(discordgo.IntentsGuildMessages)
 
-		// We will ignore the prefix case, so 'eXaMpLe!' is also a valid prefix
-		IgnorePrefixCase: true,
-
-		// We don't want bots to be able to execute our commands
-		BotsAllowed: false,
-
-		// We may initialize our commands in here, but we will use the corresponding method later on
-		Commands: []*dgc.Command{},
-
-		// We may inject our middlewares in here, but we will also use the corresponding method later on
-		Middlewares: []dgc.Middleware{},
-
-		// This handler gets called if the bot just got pinged (no argument provided)
-		PingHandler: func(ctx *dgc.Ctx) {
-			ctx.RespondText("Pong!")
-		},
-	})
-
-	// Register the default help command
-	router.RegisterDefaultHelpCommand(session, nil)
-
-	// Register a simple middleware that injects a custom object
-	router.RegisterMiddleware(func(next dgc.ExecutionHandler) dgc.ExecutionHandler {
-		return func(ctx *dgc.Ctx) {
-			// Inject a custom object into the context
-			ctx.CustomObjects.Set("myObject", 69)
-
-			// You can retrieve the object like this
-			obj := ctx.CustomObjects.MustGet("myObject").(int)
-			fmt.Println(obj)
-
-			// Call the next execution handler
-			next(ctx)
-		}
-	})
-
-	// Register a simple command that responds with our custom object
-	router.RegisterCmd(&dgc.Command{
-		// We want to use 'obj' as the primary name of the command
-		Name: "obj",
-
-		// We also want the command to get triggered with the 'object' alias
-		Aliases: []string{
-			"object",
-		},
-
-		// These fields get displayed in the default help messages
-		Description: "Responds with the injected custom object",
-		Usage:       "obj",
-		Example:     "obj",
-
-		// You can assign custom flags to a command to use them in middlewares
-		Flags: []string{},
-
-		// We want to ignore the command case
-		IgnoreCase: true,
-
-		// You may define sub commands in here
-		SubCommands: []*dgc.Command{},
-
-		// We want the user to be able to execute this command once in five seconds and the cleanup interval shpuld be one second
-		RateLimiter: dgc.NewRateLimiter(5*time.Second, 1*time.Second, func(ctx *dgc.Ctx) {
-			ctx.RespondText("You are being rate limited!")
-		}),
-
-		// Now we want to define the command handler
-		Handler: objCommand,
-	})
+	session.AddHandler(messageRespond)
 }
 
-func objCommand(ctx *dgc.Ctx) {
-	// Respond with the just set custom object
-	ctx.RespondText(strconv.Itoa(ctx.CustomObjects.MustGet("myObject").(int)))
+func messageRespond(s *discordgo.Session, m *discordgo.MessageCreate) {
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+
+	var ctx context.Context
+	var span *trace.Span
+	me := MessageEvent{m.Message, ctx}
+
+	ctx, span = StartSpanOrTraceFromMessage(&me)
+
+	for k, v := range getSessionProps(s) {
+		span.AddField(k, v)
+	}
+
+	if m.Content == "ping" {
+		s.ChannelMessageSend(m.ChannelID, "pong")
+	}
+
+}
+
+// StartSpanOrTraceFromMessage creates and returns a span for the provided MessageEvent. If
+// there is an existing span in the Context, this function will create the new span as a
+// child span and return it. If not, it will create a new trace object and return the root
+// span.
+func StartSpanOrTraceFromMessage(me *MessageEvent) (context.Context, *trace.Span) {
+	ctx := me.Context
+	span := trace.GetSpanFromContext(ctx)
+	if span == nil {
+		// there is no trace yet. We should make one! and use the root span.
+		var tr *trace.Trace
+		ctx, tr = trace.NewTrace(ctx, "")
+
+		span = tr.GetRootSpan()
+	} else {
+		// we had a parent! let's make a new child for this handler
+		ctx, span = span.CreateChild(ctx)
+	}
+	// go get any common HTTP headers and attributes to add to the span
+	for k, v := range getMessageProps(me) {
+		span.AddField(k, v)
+	}
+	return ctx, span
+}
+
+func getMessageProps(me *MessageEvent) map[string]interface{} {
+
+	messageProps := make(map[string]interface{})
+
+	messageProps["message.ID"] = me.Message.ID
+	messageProps["message.ChannelID"] = me.Message.ChannelID
+	messageProps["message.GuildID"] = me.Message.GuildID
+	messageProps["message.ActivityType"] = me.Message.Activity.Type
+	messageProps["message.AuthorID"] = me.Message.Author.ID
+	messageProps["message.AuthorUsername"] = me.Message.Author.Username
+	messageProps["message.MessageType"] = me.Message.Type
+	messageProps["message.RawContent"] = me.Message.Content
+	messageProps["message.MentionEveryone"] = me.Message.MentionEveryone
+	messageProps["message.MentionRoles"] = me.Message.MentionRoles
+
+	channels := []string{""}
+	for _, mc := range me.Message.MentionChannels {
+		channels = append(channels, mc.ID)
+	}
+
+	messageProps["message.MentionChannels"] = channels
+
+	mentions := []string{""}
+	for _, m := range me.Message.Mentions {
+		mentions = append(mentions, m.ID)
+	}
+
+	messageProps["message.Mentions"] = mentions
+
+	return messageProps
+}
+
+func getSessionProps(s *discordgo.Session) map[string]interface{} {
+	sessionProps := make(map[string]interface{})
+
+	sessionProps["session.ShardID"] = s.ShardID
 }
