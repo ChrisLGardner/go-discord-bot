d6036e3b68dc6442ae58156b491cf704f898afb5
Chris Gardner <chris@chrislgardner.dev>
2021-03-06 13:44:19 +0000

    Add automation to create new feature flags as they are added to code

---
+++ Update-FeatureFlags.ps1
@@ -0,0 +1,25 @@
+[cmdletbinding()]
+param (
+    [Parameter(Mandatory)]
+    [String]$ApiKey,
+
+    [Parameter(Mandatory)]
+    [String]$Path
+)
+
+$Headers = @{
+    authorization = "bearer $ApiKey"
+}
+
+$Uri = "https://api.optimizely.com/v2/features"
+
+$Flags = Get-Content -Path $Path -Raw | ConvertFrom-Json
+
+$Features = Invoke-RestMethod -Uri "$($Uri)?project_id=$Env:OPTIMIZELY_PROJECT" -Method GET -Headers $Headers
+
+foreach ($Flag in $Flags.Where({$_.Key -notin $Features.Key})) {
+
+    $Flag | Add-Member -Name project_id -Value ([int64]$Env:OPTIMIZELY_PROJECT) -MemberType NoteProperty
+
+    Invoke-RestMethod -Uri $Uri -Method POST -Body ($Flag | ConvertTo-Json) -Headers $Headers
+}
